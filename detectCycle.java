18104f1d07d7a6cc595401bb0313c116d8465d00
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2022-10-08 18:57:06 +0530

    ## detect & remote cylcle from list

---
+++ detectCycle.java
@@ -0,0 +1,53 @@
+
+// ##
+class Solution {
+    Node detectCycle(Node head) {
+        if (head == null)
+            return null;
+
+        Node s = head, f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+            if (s == f) {
+                s = head;
+                while (s != f) {
+                    s = s.next;
+                    f = f.next;
+                }
+                return s;
+            }
+        }
+
+        return null;
+    }
+
+    void removeCycle(Node head) {
+        if (head == null) return;
+
+        Node s = head, f = head;
+
+        while (f != null && f.next != null) {
+
+            s = s.next;
+            f = f.next.next;
+            if (s == f) {
+                s = head;
+                if (s != f) {
+                    while (s.next != f.next) {
+                        s = s.next;
+                        f = f.next;
+                    }
+                } 
+                else
+                    while (s != f.next) f = f.next;
+
+                f.next = null;
+                return;
+            }
+        }
+
+        return;
+    }
+}
