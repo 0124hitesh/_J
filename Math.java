dde66a147a1616c8c85d54a33eeadc16d4a65094
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2022-11-21 23:02:06 +0530

    Most Significant Bit, Count bit set

--- Math.java
+++ Math.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.lang.Math.*;
 class Math{
 
     // 1. 
@@ -16,7 +18,7 @@ class Math{
     // a^b in O(log n) 
     // Binary Exponentiation
     // https://cp-algorithms.com/algebra/binary-exp.html
-    // https://leetcode.com/problems/super-pow/description/
+    // https://leetcode.com/problems/super-pow/solutions/2412552/java-binary-exponentiation-euler-totient-function-etf-with-explanation/
     int pow(int a, int b){
         int res = 1;
 
@@ -30,9 +32,63 @@ class Math{
         return res;
     }
 
+    // 3. 
+    // Most Significant Bit
+    // Given a number n, find the significance of the most significant bit, i.e., 
+    // find the greatest number less than or equal to n which is the power of two.
+    // int msbSignificance(int n) {
+    //     int res = (int)(Math.log(1.0)/Math.log(2.0));
+    //      return 1 << res;
+    //  }
+
+
+    // 4.
+    // is Power of two
+    boolean isPowerOfTwo(int n) {
+        if(n == 0) return false;
+		
+		return (n & (n - 1)) == 0;
+    }
+
+
+    // 5. a
+    // Count Set Bits
+    //  count the number of set bits in the binary representation of n.
+    int countSetBits(int n){
+		int ans = 0;
+		int x = 1;
+		for(int i = 0; i < 31; ++i){
+			if((n&x) != 0){
+				x = x << 1;
+				++ans;
+			} 
+		}
+		return ans;
+    }
+
+    // 5. b
+    // Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+    // ans[i] is the number of 1's in the binary representation of i.
+    // Input: n = 2
+    // Output: [0,1,1]
+    // Explanation:
+    // 0 --> 0
+    // 1 --> 1
+    // 2 --> 10
+
+    // An easy recurrence for this problem is res[i] = res[i / 2] + i % 2.
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for(int i = 1; i <= n; ++i){
+            res[i] = res[i >> 1] + (i&1);
+        }
+
+        return res;
+    }
     public static void  main(String...args){
 
         Math o = new Math();
-        System.out.println(o.pow(2,5));
+        
+        System.out.println();
     }
 }
