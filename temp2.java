82aaee34a280c4a573cbaa33033f04d165dbdc28
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2022-11-25 23:23:17 +0530

    Divide 2 Integers

--- temp2.java
+++ temp2.java
@@ -1,16 +1,7 @@
 import java.util.*;
-
-class A {
-  
-    public static void main(String... args) {
-
-       Map<Integer, Integer> m = new HashMap<>(2);
-       m.put(1,1);
-       m.put(2,2);
-       m.put(3,3);
-       System.out.println(m);
-
-
-       System.out.println(Character.MAX_CODE_POINT);
-    }
-}
+class Solution {
+	
+    public static void main(String...args){
+		System.out.println(Integer.toBinaryString(1 << 1));
+	}
+{}}
