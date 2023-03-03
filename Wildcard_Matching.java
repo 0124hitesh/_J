4db9c3c7ffe1138b706e454d1e28981e064040f9
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2023-03-03 22:26:34 +0530

    Weighted Job Scheduling

--- Wildcard_Matching.java
+++ Wildcard_Matching.java
@@ -1,30 +1,30 @@
 class Solution {
-	boolean isMatch (String s, String p) {
-	   	int n = s.length();
-		int m = p.length();
-		boolean[][] dp = new boolean[n + 1][m + 1];
-		dp[0][0] = true;
-		for(int i = 1; i <= m; i++) {
-			if(p.charAt(i - 1) == '*') {
-				dp[0][i] = dp[0][i - 1];
-			}
-			else {
-				dp[0][i] = false;
-			}
-		}
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= m; j++) {
-				if(p.charAt(j - 1) == '?' || (p.charAt(j - 1) == s.charAt(i - 1))) {
-					dp[i][j] = dp[i - 1][j - 1];
-				}
-				else if(p.charAt(j - 1) == '*') {
-					dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
-				}
-				else {
-					dp[i][j] = false;
-				}
-			}
-		}
-		return dp[n][m];
-	}
+    public boolean isMatch(String s, String p) {
+        return check(s, p, 0, 0) == 2;
+    }
+
+    private int check(String s, String p, int i, int j){
+         if(i == s.length()){
+            if(j == p.length()) return 2;
+
+            // no need to check further if true
+            if(p.charAt(j) != '*') return 1;
+        }
+        
+        if(j == p.length()) return 0;
+
+        if(p.charAt(j) == '*'){
+            if(j + 1 < p.length() && p.charAt(j + 1) == '*')    
+                return check(s, p, i, j + 1);
+
+            for(int t = i; t <= s.length(); ++t){
+                int ans = check(s, p, t, j + 1);
+                if(ans == 1 || ans == 2) return ans;
+            }
+        }
+        if(p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))
+            return check(s, p, i + 1, j + 1);
+
+        return 0;
+    }
 }
